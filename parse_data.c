628c0b382c68eb66a0bf4dfb43c0c7d746c97777
dgabai50 <dgabai50@stuy.edu>
2025-01-09 23:27:12 -0500

    added function to handle server input

--- parse_data.c
+++ parse_data.c
@@ -1,6 +1,18 @@
 #include "parse_data.h"
+#include "pipe_networking.h"
 
-struct response parse_server_response(int *msg) {
-    struct response response;
-    return response;
+void getInput(int from, int to) {
+    int in;
+    char input[100];
+    printf("Enter your move: (Rock / Paper / Scissors\n)");
+    if (fgets(input, sizeof(input), stdin) == -1) {
+        printf("ERROR\n");
+        exit(1);
+      }
+    write(to, &input, sizeof(input));
+    read(from, &in, sizeof(in));
+    if (in != VALID) {
+        printf("INVALID INPUT\n");
+        getInput(from, to);
+    }
 }

109793c4d646dc5d94fb8db8b105aa5f3abae07b
dgabai50 <dgabai50@stuy.edu>
2025-01-13 14:46:45 -0500

    added in func headers

--- game.h
+++ game.h
@@ -2,5 +2,7 @@
 #define GAME_H
 void send_to_client(int * client, int data);
 int get_winner(int r1, int r2);
-void play_game(int * client1, int * client2, int * result);
+void play_game_server(int * client1, int * client2, int * result);
+int get_user_turn();
+void play_game_client(int to_server, int from_server);
 #endif

4112e87924df96dcd3720c33ecafb15dd17c7b71
dgabai50 <dgabai50@stuy.edu>
2025-01-16 14:25:07 -0500

    refactoring to use structs

--- game.h
+++ game.h
@@ -2,7 +2,12 @@
 #define GAME_H
 void send_to_client(int * client, int data);
 int get_winner(int r1, int r2);
-void play_game_server(int * to_client1, int * to_client2, int * from_client1, int * from_client2, int * result);
+void play_game_server(struct client client1, struct client client2, int * result);
 int get_user_turn();
 void play_game_client(int to_server, int from_server);
+struct client {
+	int to_client;
+	int from_client;
+	char user[100];
+}
 #endif

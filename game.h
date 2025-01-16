6dc260cddb22e608915ab755135b8f97cd438f0d
dgabai50 <dgabai50@stuy.edu>
2025-01-16 14:41:43 -0500

    moved more funcs to structs

--- game.h
+++ game.h
@@ -1,13 +1,14 @@
 #ifndef GAME_H
 #define GAME_H
-void send_to_client(int * client, int data);
-int get_winner(int r1, int r2);
-void play_game_server(struct client client1, struct client client2, int * result);
-int get_user_turn();
-void play_game_client(int to_server, int from_server);
 struct client {
 	int to_client;
 	int from_client;
 	char user[100];
-}
+};
+void send_to_client(int client, int data);
+int get_winner(int r1, int r2);
+void play_game_server(struct client * client1, struct client * client2, struct client * result);
+int get_user_turn();
+void play_game_client(int to_server, int from_server);
+
 #endif

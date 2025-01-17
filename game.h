a00149553257ec967e9c080b38ffa96d099570dc
dgabai50 <dgabai50@stuy.edu>
2025-01-17 12:24:22 -0500

    added header for get_username

--- game.h
+++ game.h
@@ -10,5 +10,5 @@ int get_winner(int r1, int r2);
 void play_game_server(struct client * client1, struct client * client2, struct client * result);
 int get_user_turn();
 void play_game_client(int to_server, int from_server);
-
+void get_username(struct client * client);
 #endif
